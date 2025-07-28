f8fb9a5d503692be8b231a3f2dab79bed237e129
1916-pravallika <srirampravallika2006@gmail.com>
2025-07-28 10:08:25 +0530

    deploy into gitpages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
+
 export default defineConfig({
+  base: '/reactcicdapp/',
   plugins: [react()],
 })
+
